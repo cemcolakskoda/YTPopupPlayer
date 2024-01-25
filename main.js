d3b42b249a06bbb34cee3b1221957730dec8c718
cemcolakskoda <c.colak@skoda.com.tr>
2024-01-25 15:56:01 +0300

    background playback issue solved

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ const YTPlayerOverlay = document.querySelector(".youtube-player-overlay");
 const YTLinks = document.querySelectorAll(".youtube-link");
 const YTPlayerPopup = document.querySelector(".youtube-player-popup iframe");
 
+let player; // Variable to store the YouTube player instance
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player(YTPlayerPopup, {
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    // Video is ready to play, but we won't autoplay it
+}
+
 YTLinks.forEach(link => {
     link.addEventListener("click", () => {
         YTPlayerOverlay.classList.add("active");
-        let videoLink = `https://www.youtube.com/embed/${link.dataset.link}`;
+        let videoLink = `https://www.youtube.com/embed/${link.dataset.link}?enablejsapi=1`;
         YTPlayerPopup.src = videoLink;
+
+        // Ensure the YouTube API is loaded before creating the player
+        if (typeof YT !== 'undefined' && YT.loaded) {
+            onYouTubeIframeAPIReady();
+        }
     });
 });
 
 YTPlayerOverlay.addEventListener("click", () => {
+    // Pause the video using the YouTube API when closing the overlay
+    if (player) {
+        player.pauseVideo();
+    }
+
+    // Remove the "active" class from the overlay
     YTPlayerOverlay.classList.remove("active");
 });
